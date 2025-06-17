9c816120a5a15d151934d24b41017336a66f758d
Exoticdevelopment <exoticdev777@gmail.com>
2025-06-17 12:14:08 -0500

    Add project files

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este archivo puede mantenerse vacío o agregar funcionalidades interactivas en el futuro.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("The New Religion website loaded.");
+});
